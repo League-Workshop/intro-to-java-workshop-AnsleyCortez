9679bfe73f59231a7ea3757ddfa6ff7037d18948
leaguestudent <leaguestudent@administrator-Inspiron-15-3552>
2018-08-07 11:29:19 -0700

    Day 2

--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -4,12 +4,22 @@ import java.awt.Color;
 import org.jointheleague.graphical.robot.Robot;
 
 public class MyFirstJavaProgram {
-	
+
 	public static void main(String[] args) {
-		
+
 		// START HERE
-	
-		
-		
+		Robot LIV = new Robot();
+		LIV.move(100);
+		LIV.sparkle();
+		LIV.setSpeed(100);
+		LIV.penDown();
+		LIV.setPenColor(Color.white);
+		int sides = 5;
+		int angle = 360 / sides;
+		for (int i = 0; i < sides; i++) {
+			LIV.move(100);
+			LIV.turn(angle);
+
+		}
 	}
 }
--- src/section2/Spiral.java
+++ src/section2/Spiral.java
@@ -1,25 +1,28 @@
 package section2;
 
+import java.awt.Color;
+
 import org.jointheleague.graphical.robot.Robot;
 
 public class Spiral {
 
 	public static void main(String[] args) {
 		// 1. Create a new Robot
-		
-		// 5. Set your robot's pen down 
-		
+		Robot Robot = new Robot();
+		// 5. Set your robot's pen down
+		Robot.penDown();
 		// 3. Set the robot to go at max speed (100)
-		
+		Robot.setSpeed(100);
 		// 4. Use a for loop to repeat steps #7 to #8, 75 times
-		
+		for (int i = 0; i < 75; i++) {
+
 			// 7. Change the pen color to random
-		
-			// 6. Move the robot 5 times the loop counter (5*i)
-			
+			Robot.setPenColor(Color.PINK);
+			Robot.move(i*5);
 			// 2. Turn the robot 360/7 degrees to the right
-		
+			Robot.turn(360 / 7);
 			// 8. Set the pen width to i
-			
+			Robot.setPenWidth(i);
+		}
 	}
 }
